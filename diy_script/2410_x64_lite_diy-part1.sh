8910bca8c64aa17e8ec141afb76f657323116197
gxnas <52808122+gxnas@users.noreply.github.com>
2026-03-04 18:42:40 +0800

    Update 2410_x64_lite_diy-part1.sh

--- diy_script/2410_x64_lite_diy-part1.sh
+++ diy_script/2410_x64_lite_diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i '/helloworld/d' feeds.conf.default
 sed -i '/small/d' feeds.conf.default
 sed -i '/passwall/d' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default

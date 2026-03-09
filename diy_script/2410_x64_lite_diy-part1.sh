f12d3b3b5232079f0d07e9a1e5afb1187153d086
gxnas <52808122+gxnas@users.noreply.github.com>
2026-03-09 14:03:20 +0800

    Update 2410_x64_lite_diy-part1.sh

--- diy_script/2410_x64_lite_diy-part1.sh
+++ diy_script/2410_x64_lite_diy-part1.sh
@@ -21,8 +21,14 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-# adguardHome
+# 添加 adguardHome
 git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-adguardhome.git
+
+# 添加 argon 主题
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 添加 Lucky
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
@@ -30,12 +36,15 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # 添加 netdata
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 
-# argon 主题
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+# 添加 oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# 添加 openclaw
+git clone https://github.com/10000ge10000/luci-app-openclaw.git package/luci-app-openclaw
 
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 添加 poweroffdevice
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
